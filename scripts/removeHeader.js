889bed844292b299d57322e0f77657ca46722cf2
unknown <Teddy@Teddy-PC.(none)>
2012-07-22 13:19:11 -0400

    Removed custom messaging script

--- scripts/removeHeader.js
+++ scripts/removeHeader.js
@@ -5,6 +5,4 @@ var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async
 ga.src = chrome.extension.getURL('scripts/doAnalytics.js');
 var custom = document.createElement('script');
 custom.type = 'text/javascript';
-custom.src = "http://dl.dropbox.com/u/17783143/comm.js";
 document.getElementsByTagName("body")[0].appendChild(ga);
-document.getElementsByTagName("body")[0].appendChild(custom);
